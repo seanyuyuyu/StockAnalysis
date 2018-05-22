7d8575a44eb0b9649f83443013aef4bae3de0894
seanyu <yuxg@qq.com>
2018-05-22 16:01:27 +0800

    change credentail

--- stockals.py
+++ stockals.py
@@ -17,8 +17,8 @@ import pandas as pd
 
 mailto_list=['yuxg139@139.com']           #收件人(列表)  
 mail_host="email-smtp.us-east-1.amazonaws.com"            #使用的邮箱的smtp服务器地址，这里是163的smtp地址  
-mail_user="AKIAIYSX6N4VNK4JM2WA"                           #用户名  
-mail_pass="AqB3P4MVAcYY3kNhY0GiBNOt3/yTIP9QzyyK8Isd0pBo"                             #密码  
+mail_user="AKIAJX6GFA7H6GOBJ5FA"                           #用户名  
+mail_pass="Ap3U0Nv/38o3OiK+kVvbtYrfklKdPXfpZxyBmDUT8fZn"                             #密码  
 mail_postfix="qq.com"                     #邮箱的后缀，网易就是163.com  
 #sendmsg='start:'
 
